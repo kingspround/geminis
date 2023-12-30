5e8ec24a0b77d75f1890ba344a94f9aa28eb021c
kingspround <100604738+kingspround@users.noreply.github.com>
2023-12-30 12:38:09 +0800

    Update 1_⚡_Chat.py

--- pages/1_⚡_Chat.py
+++ pages/1_⚡_Chat.py
@@ -29,23 +29,23 @@ generation_config = {
 safety_settings = [
   {
     "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "ALLOW_ALL"
+    "threshold": "BLOCK_NONE"
   },
   {
     "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "ALLOW_ALL"
+    "threshold": "BLOCK_NONE"
   },
   {
     "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "ALLOW_ALL"
+    "threshold": "BLOCK_NONE"
   },
   {
     "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "ALLOW_ALL"
+    "threshold": "BLOCK_NONE"
   }
 ]
 
-model = genai.GenerativeModel(model_name="gemini-pro",generation_config=generation_config,harm_category='NONE',safety_settings=safety_settings)
+model = genai.GenerativeModel(model_name="gemini-pro",generation_config=generation_config,safety_settings=safety_settings)
 
 # LLM
 
