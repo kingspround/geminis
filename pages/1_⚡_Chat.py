9030368c5aea250cfdd888f3a7051d99ff509fcf
kingspround <100604738+kingspround@users.noreply.github.com>
2023-12-30 18:50:17 +0800

    Update 1_⚡_Chat.py

--- pages/1_⚡_Chat.py
+++ pages/1_⚡_Chat.py
@@ -13,14 +13,14 @@ import streamlit as st
 genai.configure(api_key="st.session_state.key")
 
 key = st.text_input("Your key", type="password")    
-st.session_state.key = st.session_state.get(key, None)
+st.session_state.key = st.session_state.get(key, "AIzaSyCDoTOEe1CAMKCz4GhnCe66l5Y6pSMsIu8")
 
-st.session_state.key = st.session_state.get(key, None)
+st.session_state.key = st.session_state.get(key, "AIzaSyCDoTOEe1CAMKCz4GhnCe66l5Y6pSMsIu8")
 if not st.session_state.key: 
     st.info("Please add your key to continue.")
     st.stop()
 
-model = GenerativeModel(model_name="gemini-pro", key=st.session_state.get(key, None))
+model = GenerativeModel(model_name="gemini-pro", key=st.session_state.get(key, "mykey"))
 
 # Your code to generate content using model
 
