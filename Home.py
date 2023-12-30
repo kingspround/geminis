76dd2ef7796399051b2bfd0145e04187e94365bb
kingspround <100604738+kingspround@users.noreply.github.com>
2023-12-30 11:48:07 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,6 @@ st.config.session_state_persistence = True
 st.set_page_config(
     page_title="Gemini",
     page_icon="👋",
-    # key="AIzaSyCDoTOEe1CAMKCz4GhnCe66l5Y6pSMsIu8" # 你可以在这里添加一个key参数，用来区分不同的页面配置
 )
 
 
@@ -22,9 +21,13 @@ if "key" not in st.session_state:
     st.session_state.key = None
     
 
-key = st.sidebar.text_input("Your key", type="password", key="AIzaSyCDoTOEe1CAMKCz4GhnCe66l5Y6pSMsIu8") # 你可以在这里添加一个key参数，用来区分不同的文本输入框   
+key = st.sidebar.text_input("Your key", type="password")    
 if key:
     st.session_state.key =key
+    
+if not st.session_state.key: 
+    st.info("Please add your key to continue.")
+    st.stop()
     
 if not st.session_state.key: 
     st.info("Please add your key to continue.")
