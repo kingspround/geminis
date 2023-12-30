386acde69917ebff6c9b41704d4542e2300535ad
kingspround <100604738+kingspround@users.noreply.github.com>
2023-12-30 11:37:31 +0800

    Update 1_⚡_Chat.py

--- pages/1_⚡_Chat.py
+++ pages/1_⚡_Chat.py
@@ -29,19 +29,19 @@ generation_config = {
 safety_settings = [
   {
     "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    "threshold": "NONE"
   },
   {
     "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    "threshold": "NONE"
   },
   {
     "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    "threshold": "NONE"
   },
   {
     "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    "threshold": "NONE"
   }
 ]
 
