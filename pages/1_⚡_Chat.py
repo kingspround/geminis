cdf3dfce2d948c17dd9ebc76f7dfa3a8ea0cf297
kingspround <100604738+kingspround@users.noreply.github.com>
2024-05-21 00:41:06 +0800

    Update 1_⚡_Chat.py

--- pages/1_⚡_Chat.py
+++ pages/1_⚡_Chat.py
@@ -48,7 +48,7 @@ safety_settings = [
    },
 ]
 
-model = genai.GenerativeModel(model_name="gemini-pro-1.5",generation_config=generation_config,safety_settings=safety_settings)
+model = genai.GenerativeModel(model_name="gemini-pro",generation_config=generation_config,safety_settings=safety_settings)
 
 # LLM
 
