3a786f83248349897b34e17c0d64f37e6673e057
kingspround <100604738+kingspround@users.noreply.github.com>
2024-07-03 17:31:22 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,7 +19,7 @@ st.markdown(
 key="AIzaSyCDoTOEe1CAMKCz4GhnCe66l5Y6pSMsIu8"
 
 if "key" not in st.session_state:
-    st.session_state.key = ""
+    st.session_state.key = None
     
 
 key = st.sidebar.text_input("快说你的秘密，不然人家就踩死你~♡", type="password")    
@@ -31,8 +31,4 @@ if not st.session_state.key:
     st.info("好弱好弱~♡  真不像样~♡  快说你的秘密，不然人家就生气了！")
     st.stop()
     
-if not st.session_state.key: 
-    st.info("好弱好弱~♡  真不像样~♡  快说你的秘密，不然人家就生气了！")
-    st.stop()
-
 # ... 这里可以添加你想要的功能 ...
