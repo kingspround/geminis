5632d617169177fb82ec3f5dfba4b4f3ac151390
kingspround <100604738+kingspround@users.noreply.github.com>
2024-07-06 21:31:32 +0800

    Update 花花.py

--- pages/花花.py
+++ pages/花花.py
@@ -25,25 +25,6 @@ generation_config = {
     "top_k": 0,
     "max_output_tokens": 10000,
 }
-# 设置 gemini-pro-vision 模型的 safety_settings 为 BLOCK_NONE
-safety_settings_vision = [
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_NONE",
-    },
-]
 
 # 设置 gemini-1.5-pro-latest 模型的 safety_settings 为 BLOCK_NONE
 safety_settings_text = [
@@ -70,6 +51,26 @@ model = genai.GenerativeModel(
     generation_config=generation_config,
     safety_settings=safety_settings_text
 )
+
+# 设置 gemini-pro-vision 模型的 safety_settings 为 BLOCK_NONE
+safety_settings_vision = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+]
 model_v = genai.GenerativeModel(
     model_name='gemini-pro-vision',
     generation_config=generation_config,
