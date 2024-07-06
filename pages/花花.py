4e74765cafc8ee85928a11a6c86d0141915365f1
kingspround <100604738+kingspround@users.noreply.github.com>
2024-07-06 21:44:34 +0800

    Update 花花.py

--- pages/花花.py
+++ pages/花花.py
@@ -50,24 +50,6 @@ model = genai.GenerativeModel(
     safety_settings=safety_settings,
 )
 
-safety_settings = [
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_NONE",
-    },
-]
 model_v = genai.GenerativeModel(model_name='gemini-pro-vision', generation_config=generation_config, safety_settings=safety_settings)  # 添加 gemini-pro-vision 模型
 
 # LLM
