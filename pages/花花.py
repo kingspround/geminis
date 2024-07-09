1dc15914e9957b019aeb78d095fcd3b4978f92eb
kingspround <100604738+kingspround@users.noreply.github.com>
2024-07-09 18:36:54 +0800

    Update 花花.py

--- pages/花花.py
+++ pages/花花.py
@@ -340,7 +340,8 @@ if st.session_state.img is not None:
 if st.sidebar.button("读取历史记录"):
     try:
         with open(log_file, "rb") as f:
-            st.session_state.messages = pickle.load(f)
+            loaded_messages = pickle.load(f)  # 加载历史记录到一个新变量
+            st.session_state.messages = loaded_messages  # 将新变量赋值给 st.session_state.messages
         st.success(f"聊天记录已加载")
     except FileNotFoundError:
         st.warning("聊天记录文件不存在。")
