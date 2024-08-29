d8495060af6af9f56e05d014ec8b62803af910c6
kingspround <100604738+kingspround@users.noreply.github.com>
2024-08-29 23:34:28 +0800

    Update 1_⚡_Chat.py

--- pages/1_⚡_Chat.py
+++ pages/1_⚡_Chat.py
@@ -17,7 +17,13 @@ if not st.session_state.key:
     st.stop()
     
 genai.configure(api_key=st.session_state.key)
-
+# Set up the model
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
 safety_settings = [
   {
     "category": "HARM_CATEGORY_HARASSMENT",
