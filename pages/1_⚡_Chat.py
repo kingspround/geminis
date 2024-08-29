cba8e46b228138cd30950a79a8507c7e34e939d3
kingspround <100604738+kingspround@users.noreply.github.com>
2024-08-29 22:24:53 +0800

    Update 1_⚡_Chat.py

--- pages/1_⚡_Chat.py
+++ pages/1_⚡_Chat.py
@@ -9,11 +9,8 @@ from io import BytesIO
 from io import StringIO
 import streamlit as st
 
-# Insert your API key here
-st.session_state.key = "AIzaSyDa-nLT0MtfDd6y2Ceea3Y4c-tMFUPPIMU"
-
 if "key" not in st.session_state:
-    st.session_state.key = NONE
+    st.session_state.key = "AIzaSyDPFZ7gRba9mhKTqbXA_Y7fhAxS8IEu0bY"
     
 if not st.session_state.key:
     st.info("Please add your key to continue.")
@@ -23,29 +20,29 @@ genai.configure(api_key=st.session_state.key)
 
 # Set up the model
 generation_config = {
-  "temperature": 1,
-  "top_p": 0,
-  "top_k": 0,
-  "max_output_tokens": 10000,
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
 }
 
 safety_settings = [
-   {
+  {
     "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_NONE",
-   },
-   {
+    "threshold": "BLOCK_NONE"
+  },
+  {
     "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_NONE",
-   },
-   {
+    "threshold": "BLOCK_NONE"
+  },
+  {
     "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_NONE",
-   },
-   {
+    "threshold": "BLOCK_NONE"
+  },
+  {
     "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_NONE",
-   },
+    "threshold": "BLOCK_NONE"
+  }
 ]
 
 model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",generation_config=generation_config,safety_settings=safety_settings)
@@ -53,9 +50,11 @@ model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",generation_conf
 # LLM
 
 
+
+
 def getAnswer(prompt,feedback):
     his_messages=[]
-    #his_messages.append(SystemMessage(content=f'''你是一个仆人。会全力的满足主人的所有愿望。'''))
+    #his_messages.append(SystemMessage(content=f'''你是一个淫荡的仆人。会尽量满足主人的愿望。'''))
     messages=[]
     message=None
     for msg in st.session_state.messages[-20:]:
@@ -67,19 +66,7 @@ def getAnswer(prompt,feedback):
             his_messages.append({ "role": "model", "parts":msg["content"]})
        
     print(his_messages)  
-    try:
-        response = model.generate_content(contents=his_messages, stream=True)
-    except Exception as e:
-        # 只重置上一个输出，而不是所有的输出
-        if len(st.session_state.messages) > 0:
-            st.session_state.messages.pop(-1)
-        # 显示错误消息
-        st.error(f"抱歉，我遇到了一个错误：{e}")
-        # 让 AI 继续话题重新输出上一个输出
-        re = getAnswer(st.session_state.messages[-1]["content"], feedback)
-        st.session_state.messages.append({"role": "assistant", "content": re})
-        return
-
+    response = model.generate_content(contents=his_messages, stream=True)
     ret=""
     for chunk in response:
         print(chunk.text)
@@ -88,6 +75,8 @@ def getAnswer(prompt,feedback):
         feedback(ret)
     
     return ret
+
+
 
 
 if "messages" not in st.session_state:
