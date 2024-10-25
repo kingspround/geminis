9c4a3e6c69190800f771e332c76d16c18ada6d80
kingspround <100604738+kingspround@users.noreply.github.com>
2024-10-25 21:54:18 +0800

    Update 桦桦5.py

--- pages/桦桦5.py
+++ pages/桦桦5.py
@@ -25,7 +25,7 @@ genai.configure(api_key=st.session_state.key)
 # Set up the model
 generation_config = {
   "temperature": 1,
-  "top_p": 1,
+  "top_p": 0,
   "top_k": 1,
   "max_output_tokens": 10000,
 }
