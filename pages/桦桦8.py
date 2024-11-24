a89039977df771f415174ecb39e4190596a8c2ec
kingspround <100604738+kingspround@users.noreply.github.com>
2024-11-24 14:07:00 +0800

    Update 桦桦8.py

--- pages/桦桦8.py
+++ pages/桦桦8.py
@@ -17,7 +17,7 @@ import glob
 api_keys = {
     "主密钥": "AIzaSyCBjZbA78bPusYmUNvfsmHpt6rPx6Ur0QE",  # 替换成你的主 API 密钥
     "备用1号": "AIzaSyAWfFf6zqy1DizINOwPfxPD8EF2ACdwCaQ",  # 替换成你的备用 API 密钥
-    "备用2号":"AIzaSyD4UdMp5wndOAKxtO1CWpzuZEGEf78YKUQ"
+    "备用2号":"AIzaSyD4UdMp5wndOAKxtO1CWpzuZEGEf78YKUQ",
     "备用3号":"AIzaSyBVbA7tEyyy_ASp7l9P60qSh1xOM2CSMNw"
 }
 
