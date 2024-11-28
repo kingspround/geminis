d3deb4f1d644cc263635abe19add59aaf5a9f016
kingspround <100604738+kingspround@users.noreply.github.com>
2024-11-28 23:14:02 +0800

    Update 哗哗2.py

--- pages/哗哗2.py
+++ pages/哗哗2.py
@@ -35,7 +35,7 @@ safety_settings = [
 
 model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",generation_config=generation_config,safety_settings=safety_settings)
 
-)
+
 
 
 def getAnswer(prompt, feedback):
