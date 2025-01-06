698023cefd8a22c0b9cbb6cca9dffe236217cbf1
kingspround <100604738+kingspround@users.noreply.github.com>
2025-01-06 23:25:16 +0800

    Update 桦桦2【R18 galgame系统】.py

--- pages/桦桦2【R18 galgame系统】.py
+++ pages/桦桦2【R18 galgame系统】.py
@@ -58,7 +58,7 @@ safety_settings = [
 ]
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
+    model_name="gemini-1.5-pro",
     generation_config=generation_config,
     safety_settings=safety_settings,
 )
