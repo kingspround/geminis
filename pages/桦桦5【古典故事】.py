38887f3c2889cb9e5879a769b4d51240ade3cc43
kingspround <100604738+kingspround@users.noreply.github.com>
2025-01-06 22:21:27 +0800

    Update 桦桦5【古典故事】.py

--- pages/桦桦5【古典故事】.py
+++ pages/桦桦5【古典故事】.py
@@ -55,7 +55,7 @@ safety_settings = [
 ]
 
 model = genai.GenerativeModel(
-    model_name="gemini-2.0-flash-exp",
+    model_name="gemini-1.5-flash-latest",
     generation_config=generation_config,
     safety_settings=safety_settings,
 )
