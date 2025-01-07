62074f717ebe6201cf55d3660768532ccd19f383
kingspround <100604738+kingspround@users.noreply.github.com>
2025-01-07 10:21:49 +0800

    Update 话话3.py

--- pages/话话3.py
+++ pages/话话3.py
@@ -37,7 +37,7 @@ safety_settings = [
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
 ]
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-pro",
+    model_name="gemini-2.0-flash-exp",
     generation_config=generation_config,
     safety_settings=safety_settings,
 )
