519675833d618978fb2c48749ea7548524411fab
kingspround <100604738+kingspround@users.noreply.github.com>
2025-01-10 20:40:38 +0800

    Update 桦桦5【古典故事】.py

--- pages/桦桦5【古典故事】.py
+++ pages/桦桦5【古典故事】.py
@@ -51,7 +51,7 @@ generation_config = {
 safety_settings = [
     {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
     {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
-    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_FEW"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
 ]
 
