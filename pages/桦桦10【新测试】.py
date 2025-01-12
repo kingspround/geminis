9a2498ae1faffcb2722b246f6ba8ee60b6943f37
kingspround <100604738+kingspround@users.noreply.github.com>
2025-01-12 11:38:43 +0800

    Update 桦桦10【新测试】.py

--- pages/桦桦10【新测试】.py
+++ pages/桦桦10【新测试】.py
@@ -61,7 +61,7 @@ model = genai.GenerativeModel(
     model_name="gemini-exp-1206",
     generation_config=generation_config,
     safety_settings=safety_settings,
-    system_instruction="""{
+    system_instruction="""
     "chat_completion_source": "openai",
     "openai_model": "claude-3-5-sonnet-20241022",
     "claude_model": "claude-3-5-sonnet-20241022",
@@ -1721,7 +1721,7 @@ model = genai.GenerativeModel(
     "function_calling": false,
     "seed": -1,
     "n": 1
-}""",
+""",
 )
 
 # --- 角色设定 ---
