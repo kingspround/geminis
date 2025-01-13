1357102aeebf4fca98d8d8b822e708ae510c15e7
kingspround <100604738+kingspround@users.noreply.github.com>
2025-01-14 01:08:47 +0800

    Update 话话6.py

--- pages/话话6.py
+++ pages/话话6.py
@@ -32,7 +32,7 @@ safety_settings = [
 
 
 model = genai.GenerativeModel(
-  model_name="gemini-2.0-flash-exp",
+  model_name="gemini-2.0-flash-thinking-exp-1219",
   generation_config=generation_config,
   safety_settings=safety_settings,
   system_instruction="""
