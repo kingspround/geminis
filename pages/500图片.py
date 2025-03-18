ca2ef869b26cbd05b52a95ff96ad136acfc8b0cf
kingspround <100604738+kingspround@users.noreply.github.com>
2025-03-19 02:17:09 +0800

    Update 500图片.py

--- pages/500图片.py
+++ pages/500图片.py
@@ -49,7 +49,7 @@ safety_settings = [
 
 
 model = genai.GenerativeModel(
-  model_name="gemini-2.0-flash-exp",
+  model_name="gemini-2.0-flash-thinking-exp-01-21",
   generation_config=generation_config,
   safety_settings=safety_settings,
   system_instruction="""
