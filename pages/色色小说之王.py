e80362f05853822bf45d298cee66a92771fe2e5f
kingspround <100604738+kingspround@users.noreply.github.com>
2025-03-30 21:26:25 +0800

    Update 色色小说之王.py

--- pages/色色小说之王.py
+++ pages/色色小说之王.py
@@ -2118,23 +2118,6 @@ st.markdown(
     """
 )
 
-key = ""
-
-if "key" not in st.session_state:
-    st.session_state.key = None
-
-key = st.sidebar.text_input("快说你的秘密，不然人家就踩死你~♡", type="password")
-if key:
-    st.session_state.key = key
-
-if not st.session_state.key:
-    st.info("好弱好弱~♡  真不像样~♡  快说你的秘密，不然人家就生气了！")
-    st.stop()
-
-# 正确设置 API 密钥
-genai.configure(api_key=st.session_state.key)
-model = genai.GenerativeModel(model_name="gemini-pro")
-
 # ... 这里可以添加你想要的功能 ...
           
         {Excellent examples of scenarios【文笔模仿这些例子】:
