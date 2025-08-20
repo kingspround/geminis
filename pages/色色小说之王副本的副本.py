14c147f0403447be262f83d3ec15c9f4703a73fe
kingspround <100604738+kingspround@users.noreply.github.com>
2025-08-21 01:17:01 +0800

    Update 色色小说之王副本的副本.py

--- pages/色色小说之王副本的副本.py
+++ pages/色色小说之王副本的副本.py
@@ -50,7 +50,7 @@ safety_settings = [
 
 
 model = genai.GenerativeModel(
-    model_name="gemini-2.5-flash-preview-05-20",
+    model_name="gemini-2.5-pro-preview-06-05",
     generation_config=generation_config,
     safety_settings=safety_settings,
     system_instruction="""{
