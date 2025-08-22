0d9c9482d404b3e5a2934eef553f8999ae0d40f9
kingspround <100604738+kingspround@users.noreply.github.com>
2025-08-22 21:18:35 +0800

    Update 1实验场.py

--- pages/1实验场.py
+++ pages/1实验场.py
@@ -76,7 +76,7 @@ safety_settings = [
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
 ]
 model = genai.GenerativeModel(
-  model_name="gemini-exp-1206",
+  model_name="gemini-2.0-flash-exp",
   generation_config=generation_config,
   safety_settings=safety_settings,
   system_instruction="""
