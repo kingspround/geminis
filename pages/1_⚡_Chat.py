ee44d4a5ca66ca41b5b0fbe599e8a30bf3204a85
kingspround <100604738+kingspround@users.noreply.github.com>
2026-03-21 04:07:06 +0800

    Update 1_⚡_Chat.py

--- pages/1_⚡_Chat.py
+++ pages/1_⚡_Chat.py
@@ -12,7 +12,7 @@ from io import StringIO
 import streamlit as st
 
 if "key" not in st.session_state:
-    st.session_state.key = "AIzaSyA9p4uc3F9zS5dz-ssfohgneUeKxVUxfyc"  # 请替换为你的实际密钥
+    st.session_state.key = "AIzaSyDp6qJunrZ37Nc9ucbItUJB-L8lYXDdGgs"  # 请替换为你的实际密钥
 
     
 if not st.session_state.key:
